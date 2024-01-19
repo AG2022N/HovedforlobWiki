db58cc2d2d48de1e90f5ece10c237649a18bd4ad
AG2022N <118338147+AG2022N@users.noreply.github.com>
2024-01-19 09:04:12 +0100

    Create InsertIntoLærer.sql

---
+++ eksempler/SQL/InsertIntoLærer.sql
@@ -0,0 +1,4 @@
+INSERT INTO Lærer (lærer_id, fornavn, efternavn, adresse, post_nr)
+VALUES ('1', 'Tom',   'It',          'Sankt Thomas Alle 3',  '1824'),
+       ('2', 'Lars',  'Henriksen',   'Nisserdalen 76',       '2740'),
+       ('3', 'Mia',   'Hansen',      'Østervej 126',         '2750'),
