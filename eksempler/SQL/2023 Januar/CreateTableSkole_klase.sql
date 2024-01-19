b96c17750abb7344384ff33e5302f8288170320b
AG2022N <118338147+AG2022N@users.noreply.github.com>
2024-01-19 09:17:29 +0100

    Add files via upload

---
+++ eksempler/SQL/2023 Januar/CreateTableSkole_klase.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Skole_klasse (
+  lokale_id int NOT NULL,
+  klasse varchar(40),
+  PRIMARY KEY (lokale_id),
+  );
---
+++ eksempler/SQL/2023 Januar/InsertIntoElev.sql
@@ -0,0 +1,10 @@
+INSERT INTO Elev (elev_id, fornavn, efternavn, adresse, post_nr, lokale_id)
+VALUES
+  ('1',  'Bo',           'Andersen',     'Gammel Byvej 12',       '2650',  '1'),
+  ('2',  'Frederikke',   'Hansen',       'Amager Boulevard 5',    '2300',  '2'),
+  ('3',  'Jens',         'Mikkelsen',    'Lily Brogbergsvej 17',  '2500',  '3'),
+  ('4',  'Phillip',      'Mortensen',    'Brunevang 90',          '2610',  '3'),
+  ('5',  'Kasper',       'Frederiksen',  'Brygertorvet 32',       '3650',  '2'),
+  ('6',  'Milla',        'Jørgensen',    'Virum Torv 25',         '2830',  '1'),
+  ('7',  'Fie',          'Knudsen',      'Allen 86',              '2770',  '1'),
+  ('8',  'Henrik',       'Madsen',       'Lily Brogbergsvej 53',  '2500',  '2');
